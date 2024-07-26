8c08f751b8cf683bc52d746d1c501ea68624a64f
kaohaohan <haohan920@gmail.com>
2024-07-26 13:09:29 -0400

    add herf

--- app/component/MemberCard.tsx
+++ app/component/MemberCard.tsx
@@ -50,7 +50,7 @@ const MemberCard: React.FC<Member> = ({ name, imageUrl, title, status }) => {
           </div> */}
           <div className="-ml-px flex w-0 flex-1">
             <a
-              href="#"
+              href="/coach/members/123"
               className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
             >
               <FireIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
